3e837d5bce6c7e027ff3ec0572ca76f1845d58c1
Xiao <1@xiao.studio>
2022-10-10 14:42:32 -0400

    Git update

---
+++ Plugins/HeroesBuilderPlugin/Intermediate/Build/Mac/x86_64/UnrealGame/Inc/HeroesBuilderPlugin/HeroesBuilder_GridTile.generated.h
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HEROESBUILDERPLUGIN_HeroesBuilder_GridTile_generated_h
+#error "HeroesBuilder_GridTile.generated.h already included, missing '#pragma once' in HeroesBuilder_GridTile.h"
+#endif
+#define HEROESBUILDERPLUGIN_HeroesBuilder_GridTile_generated_h
+
+#define FID_HostProject_Plugins_HeroesBuilderPlugin_Source_HeroesBuilderPlugin_Public_HeroesBuilder_GridTile_h_18_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FHeroesBuilder_GridTile_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> HEROESBUILDERPLUGIN_API UScriptStruct* StaticStruct<struct FHeroesBuilder_GridTile>();
+
+#define FID_HostProject_Plugins_HeroesBuilderPlugin_Source_HeroesBuilderPlugin_Public_HeroesBuilder_GridTile_h_82_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FHeroesBuilder_GridTileInfo_Statics; \
+	static class UScriptStruct* StaticStruct(); \
+	typedef FHeroesBuilder_GridTile Super;
+
+
+template<> HEROESBUILDERPLUGIN_API UScriptStruct* StaticStruct<struct FHeroesBuilder_GridTileInfo>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_HeroesBuilderPlugin_Source_HeroesBuilderPlugin_Public_HeroesBuilder_GridTile_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
